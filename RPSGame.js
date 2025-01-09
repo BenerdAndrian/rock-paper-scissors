d107763092790f670953993dafca0f39eac51bc7
vboxuser <ben527466@gmail.com>
2025-01-09 22:44:04 +0700

    adjust the name of the variables so that i could go along with what The Odin Project said
    however, i have adjusted a little on how we score and inform a winner, that instead of finding out a winner who has more score after 5 rounds, we will play until a player or computer reach 3 scores first

--- RPSGame.js
+++ RPSGame.js
@@ -2,7 +2,7 @@
 
 
 
- computerChoosing=()=>{
+ getComputerChoice=()=>{
     const choice=Math.random();
     if(choice>=0 && choice <=1/3){
         return "rock";
@@ -13,12 +13,15 @@
     }
 }
 
-let computerScore=0;
-let humanScore=0;
+getHumanChoice=()=>{
+   let humanChoice;
+    do{
+     humanChoice=prompt("Moi Nguoi Dung Nhap Mot trong Ba lua Chon Sau: rock,paper,scissors");
+    }while(humanChoice.toLowerCase()!=="rock"&&humanChoice.toLowerCase()!=="paper"&&humanChoice.toLowerCase()!=="scissors");
+    return humanChoice;
+}
 
-function compareResult(computerChoice,humanChoice){
-        
-    
+function playRound(computerChoice,humanChoice){
     if(computerChoice.toLowerCase()==="rock"){
         if(humanChoice.toLowerCase()==="rock"){
             console.log("Hoa Nhau");
@@ -56,23 +59,23 @@ function compareResult(computerChoice,humanChoice){
     console.log("Ti so: Human "+humanScore+" : "+computerScore+" Computer.")
    
 }
-
+let computerScore=0;
+let humanScore=0;
 function lastResult(){
     
     while(computerScore<3 && humanScore <3){
-        do{
-            humanChoice=prompt("Moi Nguoi Dung Nhap Mot trong Ba lua Chon Sau: rock,paper,scissors");
-        }while(humanChoice.toLowerCase()!=="rock"&&humanChoice.toLowerCase()!=="paper"&&humanChoice.toLowerCase()!=="scissors")
+     const computerChoice=getComputerChoice();
+      const humanChoice=getHumanChoice();
         // const humanChoice=prompt("Moi Nguoi Dung Nhap Mot trong Ba lua Chon Sau: rock,paper,scissors");
-        const computerChoice=computerChoosing();
-        compareResult(computerChoice,humanChoice);
+       
+       playRound(computerChoice,humanChoice);
     }
     if(computerScore===3){
-        console.log("May Tinh Thang Game Dau.");
+        console.log("May Tinh Thang Vong Dau.");
         return;
     }else if(humanScore===3){
-        console.log("Ban Chien Thang Game Dau.")
+        console.log("Ban Chien Thang Vong Dau.")
         return;
     }
 }
-lastResult(computerScore,humanScore);
+lastResult();
