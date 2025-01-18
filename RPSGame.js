e36c66964582a232dfb584f8382e63d959c584be
vboxuser <ben527466@gmail.com>
2025-01-18 18:35:51 +0700

    dfd

--- RPSGame.js
+++ RPSGame.js
@@ -1,6 +1,7 @@
 
 
-
+let computerScore=0;
+let humanScore=0;
 
  getComputerChoice=()=>{
     const choice=Math.random();
@@ -13,69 +14,80 @@
     }
 }
 
-getHumanChoice=()=>{
-   let humanChoice;
-    do{
-     humanChoice=prompt("Moi Nguoi Dung Nhap Mot trong Ba lua Chon Sau: rock,paper,scissors");
-    }while(humanChoice.toLowerCase()!=="rock"&&humanChoice.toLowerCase()!=="paper"&&humanChoice.toLowerCase()!=="scissors");
-    return humanChoice;
-}
-
-function playRound(computerChoice,humanChoice){
+// getHumanChoice=()=>{
+//    let humanChoice;
+//     do{
+//      humanChoice=prompt("Moi Nguoi Dung Nhap Mot trong Ba lua Chon Sau: rock,paper,scissors");
+//     }while(humanChoice.toLowerCase()!=="rock"&&humanChoice.toLowerCase()!=="paper"&&humanChoice.toLowerCase()!=="scissors");
+//     return humanChoice;
+// }
+function playRound(humanChoice){
+    const infoBoard=document.getElementById("infoBoard");
+    let result='';
+    const computerChoice=getComputerChoice();
+    infoBoard.innerHTML=`<p> computer chose ${computerChoice}</p>`;
     if(computerChoice.toLowerCase()==="rock"){
         if(humanChoice.toLowerCase()==="rock"){
             console.log("Hoa Nhau");
+            result = "tie this round";
         }else if(humanChoice.toLowerCase()==="paper"){
             console.log("Ban Thang Van Nay.");
             humanScore++;
+            result= "you win this round";
         }else if(humanChoice.toLowerCase()==="scissors"){
             console.log("May Tinh Thang Van Nay.");
             computerScore++;
+            result="computer wins this round.";
         }
     }
     else if(computerChoice.toLowerCase()==="paper"){
         if(humanChoice.toLowerCase()==="rock"){
             console.log("May Tinh Thang Van Nay.");
             computerScore++;
+            result="computer wins this round.";
         }else if(humanChoice.toLowerCase()==="paper"){
             console.log("Hoa Nhau")
+            result="tie this round."
         }else if(humanChoice.toLowerCase()==="scissors"){
             console.log("Ban Thang Van Nay.");
             humanScore++;
+            result="you win this round."
         }
     }
     else if(computerChoice.toLowerCase()==="scissors"){
         if(humanChoice.toLowerCase()==="rock"){
             console.log("Ban Thang Van Nay.");
             humanScore++;
+            result="you win this round";
         }else if(humanChoice.toLowerCase()==="paper"){
             console.log("May Tinh Thang Van Nay.");
             computerScore++;
+            result="computer wins this round."
         }else if(humanChoice.toLowerCase()==="scissors"){
             console.log("Hoa Nhau")
+            result="tie this round";
         }
     }
-    console.log("May Tinh Chon: "+computerChoice);
-    console.log("Ti so: Human "+humanScore+" : "+computerScore+" Computer.")
+    infoBoard.innerHTML+=`<p>${result}</p><br>`
+    
    
 }
-let computerScore=0;
-let humanScore=0;
-function lastResult(){
-    
-    while(computerScore<3 && humanScore <3){
-     const computerChoice=getComputerChoice();
+
+function playGame(){
+    let i=1;
+    while(i<=5){
       const humanChoice=getHumanChoice();
         // const humanChoice=prompt("Moi Nguoi Dung Nhap Mot trong Ba lua Chon Sau: rock,paper,scissors");
        
        playRound(computerChoice,humanChoice);
+       i++;
     }
-    if(computerScore===3){
-        console.log("May Tinh Thang Vong Dau.");
-        return;
-    }else if(humanScore===3){
-        console.log("Ban Chien Thang Vong Dau.")
-        return;
-    }
+   if(computerScore>humanScore){
+    console.log("Computer wins.");
+   }else if(computerScore<humanScore){
+    console.log("Human wins.");
+   }else if(computerScore===humanScore){
+    console.log("Tie.");
+   }
 }
-lastResult();
+
